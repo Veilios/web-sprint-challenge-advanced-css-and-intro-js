52dd5abbeafcc19e36f3770011a4fd1ca0927649
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-06-26 21:15:03 -0400

    Started task 4. Moved on and completed task 5, creating a function to remove an artist from the array

--- index.js
+++ index.js
@@ -234,11 +234,11 @@ function getArtistByIndex(array, index) {
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/* Code here */){
-
-  /* Code here */
-
-}
+// function get20s(array, data){
+//   for(let i = 0; i < array.length; i++){
+//     if(array[i])
+//   }
+// }
 
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
@@ -251,11 +251,12 @@ function get20s(/* Code here */){
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*code here*/) {
-    /* code here */
+function removeArtist(array, index) {
+  array.splice(index, 1);
+  console.log(array);
   }
   
- 
+ removeArtist(artists, 0);
 
 /**
 /* Task 6: Create a function called `addArtist` that can accept an object of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
