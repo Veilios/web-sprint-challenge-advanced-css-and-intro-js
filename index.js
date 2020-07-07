b88fc5971c8f89bf685f40f84b4ebdf1d96e2277
Alan Paul Mir <alanpaulphotos@gmail.com>
2020-07-06 20:15:03 -0400

    Finished task 4, creating a function to get all artists that worked in the 20th century

--- index.js
+++ index.js
@@ -234,15 +234,19 @@ function getArtistByIndex(array, index) {
 
 /* Task 4: Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(years){
+function get20s(artists){
   the20s = [];
-  for(let i = 0; i < array.length; i++){
-    parseInt(artists[i].years);
-    if(artists[i].years > 1900){
-      
-    }
+  num = [];
+  for(let i = 0; i < artists.length; i++){
+    num = parseInt(artists[i].years);
+    if(num > 1900){
+      the20s.push(artists[i].name);
+    }   
   }
+  return the20s;
 }
+
+console.log(get20s(artists));
 
 /* Task 5: Create a function called `removeArtist` that takes two arguments:
  *     (1) artists array
